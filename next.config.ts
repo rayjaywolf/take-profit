2db0f956d751ca215b49712e5e6f8ad934d37c5f
rayjay8 <rayjay9@proton.me>
2025-08-16 15:55:19 +0530

    revert

--- next.config.ts
+++ next.config.ts
@@ -7,15 +7,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client"],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push("@prisma/client");
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
